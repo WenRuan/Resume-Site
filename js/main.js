8b34757cdc96a245a671c13130b3fe25a06f50cf
Minishorts <slimpandaw@gmail.com>
2020-02-21 18:20:36 -0800

    Added flicker

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-var i = 0;
-var txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmo tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-var speed = 20;
+//Flicker for terminal
 
-function typeWriter() {
-	if (i < txt.length){
-		document.getElementById("text-type").innerHTML += txt.charAt(i);
-		i++;
-		setTimeout(typeWriter, speed);
+var filled = true;
+
+$(document).ready(function(){
+
+	function flicker(){
+
+		var cursor = $("#insertFlicker");
+		
+		if(filled == true)
+		{
+			cursor.html("&#9647")
+		}
+		else
+		{
+			cursor.html("&#9646")
+		}
+		filled = !filled;
 	}
-}
+
+	setInterval(flicker, 700);
+});
